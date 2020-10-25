d53fc69d9a1d8299feb86a99d40ba59e508221fc
Administrator <1260764948@qq.com>
2020-10-25 18:55:41 +0800

    提交测试

---
+++ JavaEEstudy/src/com/rth/study/test.java
@@ -0,0 +1,11 @@
+package com.rth.study;
+
+/**
+ * @author RTH
+ * @date 2020/10/25-18:40
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("HelloWord!");
+    }
+}
